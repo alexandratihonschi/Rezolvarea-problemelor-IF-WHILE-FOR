3f25a11c9c793d8d528aa7e7088c82d5f8f00581
Alexandra Țihonschi <70908434+alexandratihonschi@users.noreply.github.com>
2020-11-17 09:41:01 +0300

    Add files via upload

---
+++ problema 4.py
@@ -0,0 +1,7 @@
+from fractions import Fraction 
+m=int(input("primul numarator="))
+n=int(input("primul numitor="))
+l=int(input("al doilea numarator="))
+k=int(input("al doilea numitor="))
+print(Fraction(m,n)+Fraction(l,k))
+print(Fraction(m,n)*Fraction(l,k))
