b81898405d6a8df57b735da1fb3424d8b6ee8058
Alexandra Țihonschi <70908434+alexandratihonschi@users.noreply.github.com>
2020-11-17 21:25:13 +0300

    Add files via upload

---
+++ problema 5.py
@@ -0,0 +1,26 @@
+a=int(input("anul a-"))
+if a%12==0:
+    print("maimuta")
+if a%12==1:
+    print("cocos")
+if a%12==2:
+    print("caine")
+if a%12==3:
+    print("porc")
+if a%12==4:
+    print("sobolan")
+if a%12==5:
+    print("bou")
+if a%12==6:
+    print("tigru")
+if a%12==7:
+    print("iepure")
+if a%12==8:
+    print("dragon")
+if a%12==9:
+    print("sarpe")
+if a%12==10:
+    print("cal")
+if a%12==11:
+    print("oaie")
+    
